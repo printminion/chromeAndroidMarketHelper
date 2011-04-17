1188387b16c7359fcf361b34554d84040f1bfa9f
russenreaktor <cr@russenreaktor.de>
2011-04-17 21:25:13 +0200

    added qrcode ingection into market site

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -8,5 +8,15 @@ chrome.extension.sendRequest({
 
 function fetchTabInfo(backgroundPage) {
 	console.log('content_scripts.fetchTabInfo:' + backgroundPage);
-
+	
+    var placeholderObj = document.querySelector("#details-tab-1 div div");
+    
+	if (placeholderObj) {
+		var img = document.createElement("img");
+		img.src = 'http://chart.apis.google.com/chart?chs=110x110&cht=qr&chld=M|1&chl=' + encodeURIComponent('market://search?q=pname:' + selectedPacketName);
+		img.alt = 'Scan me';
+			
+		placeholderObj.appendChild(img);
+	}
+	
 }
